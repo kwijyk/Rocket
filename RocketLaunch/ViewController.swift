f21bc482f2744cfc14e9806114dd90425efefa19
Sergey <ser.olegov@gmail.com>
2017-10-10 23:02:04 +0300

    Functionality connected

--- RocketLaunch/ViewController.swift
+++ RocketLaunch/ViewController.swift
@@ -11,19 +11,28 @@ import UIKit
 class ViewController: UIViewController {
     override var preferredStatusBarStyle: UIStatusBarStyle {return .lightContent}
     
+    @IBOutlet weak var rocketTopConstraint: NSLayoutConstraint!
     @IBOutlet weak var launchModeView: UIView!
     @IBOutlet weak var launchLabel: UILabel!
     @IBOutlet weak var onLabel: UILabel!
-     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setNeedsStatusBarAppearanceUpdate()
     }
 
     @IBAction func launchPressed(_ sender: Any) {
+        launchModeView.isHidden = false
         
+        UIView.animate(withDuration: 3.0, animations: {
+            self.rocketTopConstraint.constant = 50
+            self.view.layoutIfNeeded()
+            
+        }) { _ in
+            self.launchLabel.isHidden = false
+            self.onLabel.isHidden = false
+        }
     }
     
-
 }
 
