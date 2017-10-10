e7b3a0577bcb206b21aa15f03dac8b42a426cb16
Sergey <ser.olegov@gmail.com>
2017-10-10 22:13:41 +0300

    Visual interface dane

--- RocketLaunch/ViewController.swift
+++ RocketLaunch/ViewController.swift
@@ -12,14 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.setNeedsStatusBarAppearanceUpdate()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+   override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
 
 }
 
